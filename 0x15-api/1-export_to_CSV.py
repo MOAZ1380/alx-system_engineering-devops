2e29331e5703abeb3fad8f513f8dd3739027de2c
moaz <moaz469654@gmail.com>
2024-08-10 05:29:41 -0700

    g

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,14 +9,10 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     user_name = user.get("username")
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    
+
     with open(f'{user_id}.csv', "w", newline="") as file:
-            writer = csv.writer(file,quoting=csv.QUOTE_ALL)
-            for t in todos:
-                writer.writerow(
-                    [ user_id,
-                    user_name,
-                    t.get('completed'),
-                    t.get("title")
-                ]
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for t in todos:
+            writer.writerow(
+                [user_id, user_name, t.get('completed'), t.get("title")]
                         )
